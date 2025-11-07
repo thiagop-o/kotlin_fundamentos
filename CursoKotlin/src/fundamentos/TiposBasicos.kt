27e3237abd8b515baeb350f3566d57d59bceb776
Thiago Oliveira <thiagop_o@hotmail.com>
2025-11-07 02:47:57 -0300

    feat: 8 tipos basicos

---
+++ CursoKotlin/src/fundamentos/TiposBasicos.kt
@@ -0,0 +1,23 @@
+package fundamentos
+
+fun main() {
+    // Tipos numericos inteiros
+    val num1: Byte = 127
+    val num2: Short = 32_767
+    val num3: Int = 2_147_483_647
+    val num4: Long = 9_223_372_036_854_775_807
+
+    // Tipos numericos reais
+    val num5: Float = 3.14F
+    val num6: Double = 3.14
+
+    // Tipo caractere
+    val char: Char = '?'
+
+    //Tipo boleano
+    val boolean: Boolean = true
+
+    print(listOf(num1, num2, num3, num4, num5, num6, char, boolean))
+
+
+}
