af7368a6558d802478399beaf4a7302c24316ff4
Thiago Pereira de Oliveira <thiagop_o@hotmail.com>
2025-11-28 00:39:35 -0300

    feat: buscando segundo elemento

---
+++ CursoKotlin/src/fundamentos/funcoes/SegundoElementoList.kt
@@ -0,0 +1,8 @@
+package fundamentos.funcoes
+
+fun <E> List<E>.secondOrNull(): E? = if (this.size >= 2) this[1] else null
+
+fun main() {
+    val list = listOf("Jao", "Maria", "Pedro")
+    println(list.secondOrNull())
+}
