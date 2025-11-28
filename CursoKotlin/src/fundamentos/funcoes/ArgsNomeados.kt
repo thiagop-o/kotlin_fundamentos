fb7aa37c1a27f6c5dabb5b6d0c84fd3f8129106b
Thiago Pereira de Oliveira <thiagop_o@hotmail.com>
2025-11-27 23:56:41 -0300

    feat: argumentos nomeados

---
+++ CursoKotlin/src/fundamentos/funcoes/ArgsNomeados.kt
@@ -0,0 +1,10 @@
+package fundamentos.funcoes
+
+fun relacaoTrabalho(chefe: String, funcionario: String): String {
+    return "funcionario $funcionario é subordidado de $chefe"
+}
+
+fun main() {
+    println(relacaoTrabalho("Joao", "Maria"))
+    println(relacaoTrabalho(funcionario = "Carlos", chefe = "Roberto"))
+}
