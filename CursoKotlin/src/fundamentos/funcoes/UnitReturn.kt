2d82636973558a2df114d5cb90f6e6d35c803df3
Thiago Pereira de Oliveira <thiagop_o@hotmail.com>
2025-11-29 01:07:24 -0300

    feat: unit return

---
+++ CursoKotlin/src/fundamentos/funcoes/UnitReturn.kt
@@ -0,0 +1,22 @@
+package fundamentos.funcoes
+
+import kotlin.math.max
+
+fun imprimirMaior1(a:Int, b:Int){
+    println(max(a, b))
+}
+
+fun imprimirMaior2(a:Int, b:Int) {
+    println(max(a, b))
+}
+
+fun imprimirMaior3(a:Int, b:Int) {
+    println(max(a, b))
+    return
+}
+
+fun main() {
+    imprimirMaior1(4,6)
+    imprimirMaior2(4,6)
+    imprimirMaior3(4,6)
+}
