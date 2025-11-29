eda9ddb2ada379427b0164590ab985b688835332
Thiago Pereira de Oliveira <thiagop_o@hotmail.com>
2025-11-29 01:16:36 -0300

    feat: funcao como parametro 1

---
+++ CursoKotlin/src/fundamentos/funcoes/FuncaoComoParametro1.kt
@@ -0,0 +1,17 @@
+package fundamentos.funcoes
+
+class Operacoes {
+    fun somar(a:Int, b:Int):Int = a+b
+
+}
+
+fun somar(a:Int, b:Int):Int = a+b
+
+fun calc(a:Int, b:Int, funcao: (Int, Int) -> Int): Int {
+    return funcao(a, b)
+}
+
+fun main() {
+    println(calc(2,3, Operacoes()::somar))
+    println(calc(2,3, ::somar))
+}
