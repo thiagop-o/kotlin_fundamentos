c9e493e4b09524d4e0e4c5f94967b43240845f87
Thiago Pereira de Oliveira <thiagop_o@hotmail.com>
2025-11-30 19:13:02 -0300

    feat: funcao como parametro 2

---
+++ CursoKotlin/src/fundamentos/funcoes/FuncaoComoParametro2.kt
@@ -0,0 +1,20 @@
+package fundamentos.funcoes
+
+fun <E> filtrar(lista: List<E>, filtro: (E) -> Boolean): List<E>  {
+    val listaFiltrada = ArrayList<E>()
+    for (e in lista){
+        if(filtro(e)){
+            listaFiltrada.add(e)
+        }
+    }
+    return listaFiltrada
+}
+
+fun comTresLetras(nome:String): Boolean{
+    return nome.length == 3
+}
+
+fun main() {
+    val nomes = listOf("Ana", "Pedro", "Gui", "Maria", "Bia")
+    println(filtrar(nomes, ::comTresLetras))
+}
