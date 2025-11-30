92618012d531e942a7425e276ae9b30f2bc3e579
Thiago Pereira de Oliveira <thiagop_o@hotmail.com>
2025-11-30 19:13:09 -0300

    feat: inline

---
+++ CursoKotlin/src/fundamentos/funcoes/Inline1.kt
@@ -0,0 +1,17 @@
+package fundamentos.funcoes
+
+inline fun transacao (funcao: () -> Unit){
+    println("Abrindo transacao...")
+    try {
+        funcao()
+    } finally {
+        println("Encerrando transacao....")
+    }
+}
+
+fun main() {
+    transacao {
+        println("Executando SQL 1")
+        println("Executando SQL 2")
+    }
+}
